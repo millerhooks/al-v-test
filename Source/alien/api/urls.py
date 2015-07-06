da9a24e1ddcf66ce226a721918214bd1f37dd472
Miller Hooks <miller@edepo.com>
2015-07-06 09:09:51 -0500

    added a little regex to the api URLs for getting details on an IP. Don't have access to actual JSON.

--- Source/alien/api/urls.py
+++ Source/alien/api/urls.py
@@ -4,5 +4,5 @@ import views
 
 urlpatterns = [
     url(r'^$', views.APIRoot.as_view(), name='api_root'),
-    url(r'^threat/details/$', views.IPDetailsView.as_view(), name='threat_details'),
+    url(r'^threat/details/(?P<ip>[a-zA-Z0-9.]+)/$', views.IPDetailsView.as_view(), name='threat_details'),
 ]
--- Source/alien/api/views.py
+++ Source/alien/api/views.py
@@ -2,18 +2,38 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import status
-from threat import *
-from serializers import *
+from threat import IPDetails, Reputation
+from serializers import DetailsSerializer
+from rest_framework import authentication, permissions
 import time
 
 
 class APIRoot(APIView):
+    """
+    API request for self details
+
+    """
+
+    # authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.AllowAny,)
+
     def get(self, request):
         return Response({
             'IP Details': reverse('threat_details', request=request),
         })
 
 class IPDetailsView(APIView):
+    """
+    Get details for IP in URL querystring.
+
+    example:
+    http://localhost/api/v1/threat/details/69.43.161.174/
+
+    """
+
+    # authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.AllowAny,)
+
     def get(self, request, *args, **kw):
         ip = request.GET.get('ip', "")
         details_request = IPDetails(ip, *args, **kw)
